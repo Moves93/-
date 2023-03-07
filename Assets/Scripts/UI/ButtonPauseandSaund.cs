0640dcdca6d79e9bc4dcfad7ac912e986992467a
Moves93 <Moves.ez@gmail.com>
2023-03-07 16:55:56 +0300

    07.03.2023
    07.03.2023

---
+++ Assets/Scripts/UI/ButtonPauseandSaund.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonPauseandSaund : MonoBehaviour
+{
+    [SerializeField] private Button _pauseButton;
+    [SerializeField] private PauseScreen _gamePauseScreen;
+
+
+    private void OnEnable()
+    {
+        
+        _pauseButton.onClick.AddListener(OnPauseButtonClick);
+    }
+
+    private void OnDisable()
+    {
+       
+        _pauseButton.onClick.RemoveListener(OnPauseButtonClick);
+    }
+
+    private void OnPauseButtonClick()
+    {
+        Time.timeScale = 0;
+        _gamePauseScreen.gameObject.SetActive(true);
+    }
+
+}
---
+++ Assets/Scripts/UI/PauseScreen.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseScreen : MonoBehaviour
+{
+    [SerializeField] private Button _restartButton;
+    [SerializeField] private Button _quitButton;
+    [SerializeField] private Button _continueButton;
+    [SerializeField] private PauseScreen _gamePauseScreen;
+
+    private void Start()
+    {
+        _gamePauseScreen.gameObject.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        _restartButton.onClick.AddListener(OnRestartButtonClick);
+        _quitButton.onClick.AddListener(OnQuitButtonClick);
+        _continueButton.onClick.AddListener(OnContinueButtonClick);
+    }
+
+    private void OnDisable()
+    {
+        _restartButton.onClick.RemoveListener(OnRestartButtonClick);
+        _quitButton.onClick.RemoveListener(OnQuitButtonClick);
+        _continueButton.onClick.RemoveListener(OnContinueButtonClick);
+    }
+
+    private void OnRestartButtonClick()
+    {
+        Time.timeScale = 1f;
+        ObjectPool.score = 0;
+        SceneManager.LoadScene(1);
+    }
+
+    private void OnQuitButtonClick()
+    {
+        ObjectPool.score = 0;
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+    }
+
+    private void OnContinueButtonClick()
+    {
+        _gamePauseScreen.gameObject.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+
+
+}
